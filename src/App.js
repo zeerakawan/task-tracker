2f62437e72b6762d300e5e7a2e22883d1a0419c0
zee1997 <awan.zeerak27@gmail.com>
2021-03-24 15:29:26 +0500

    Added condition toggling on Add Task button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
+  // Toggle button to show the add task form
+  const [showAddTask, setshowAddTask] = useState(false)
+
   // Task Data rendering on the page 
   const [tasks, setTasks] = useState([
     {
@@ -46,8 +49,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header /> 
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={() => setshowAddTask(!showAddTask)} showAdd={showAddTask}/> 
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ?
         (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>)
         : (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,10 @@
 import Button from './Button';
 
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log('Clicked')
-    }
-
+const Header = ({ title, onAdd, showAdd}) => {
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Hit Me' onClick={onClick}/>
+            <Button color='green' text={showAdd ? 'Close' : 'Add Task'} onClick={onAdd}/>
         </header>
     )
 }
