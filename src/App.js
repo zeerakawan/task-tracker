43a77196f31cf85f50d9a6561ead2d282ad07958
zee1997 <awan.zeerak27@gmail.com>
2021-03-24 15:02:04 +0500

    Adding Task Tracker files into repository

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Header from "./components/Header";
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
+  // Task Data rendering on the page 
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30 pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Brunch',
+      day: 'Feb 6th at 2:30 pm',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Play Football',
+      day: 'Feb 7th at 2:30 pm',
+      reminder: false
+    },
+  ])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000)+1
+    console.log(id)
+    const  newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
+  // For Delete
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // For Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header /> 
+      <AddTask onAdd={addTask}/>
+      {tasks.length > 0 ?
+        (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>)
+        : (
+          "Nothing To Show"
+        )
+      }
     </div>
   );
 }
